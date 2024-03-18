4cc0dfef190cbe90b659f93d1837b50b418ad305
Can Namlı <78431281+MuhammetCanNamli@users.noreply.github.com>
2024-03-18 12:30:28 +0300

    Update 13_Weather_Observation_Station_16.sql

--- sql/aggregation/13_Weather_Observation_Station_16.sql
+++ sql/aggregation/13_Weather_Observation_Station_16.sql
@@ -7,13 +7,13 @@ The STATION table is described as follows:
 	    STATION
  ______________ ______________
 | Field	       | Type	      |
--------------------------------
+ -------------- --------------
 | ID	       | NUMBER	      |
 | CITY	       | VARCHAR2(21) |
 | STATE	       | VARCHAR2(2)  |
 | LAT_N        | NUMBER	      |
 | LONG_W       | NUMBER	      |
--------------------------------
+ -------------- --------------
 
 Where LAT_N is the northern latitude and LONG_W is the western longitude.
 
