2411260ae43a6bbbab1e44f0e77b7e4c4062b654
Can Namlı <78431281+MuhammetCanNamli@users.noreply.github.com>
2024-03-18 14:07:00 +0300

    Upload Alternative Queries Solutions

---
+++ sql/alternative-queries/2_Draw_The_Triangle_2.sql
@@ -0,0 +1,20 @@
+/*
+
+P(R) represents a pattern drawn by Julia in R rows. The following pattern represents P(5):
+
+* 
+* * 
+* * * 
+* * * * 
+* * * * *
+
+Write a query to print the pattern P(20).
+
+*/
+
+-- Working Platforms: MySQL
+
+SET @TEMP:=0; 
+SELECT REPEAT('* ', @TEMP:= @TEMP + 1) 
+FROM INFORMATION_SCHEMA.TABLES
+WHERE @TEMP < 20;
