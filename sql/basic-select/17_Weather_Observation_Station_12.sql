f9bd3fcf3628570e034adcb1fcabff01e33fcfbb
Can NAMLI <namlimuhammetcan55@gmail.com>
2024-03-18 10:06:14 +0300

    'basic select' folder moved into 'sql' folder

---
+++ sql/basic-select/17_Weather_Observation_Station_12.sql
@@ -0,0 +1,24 @@
+/*
+Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
+
+The STATION table is described as follows:
+
+	    STATION
+ ______________ ______________
+| Field	       | Type	      |
+-------------------------------
+| ID	       | NUMBER	      |
+| CITY	       | VARCHAR2(21) |
+| STATE	       | VARCHAR2(2)  |
+| LAT_N        | NUMBER	      |
+| LONG_W       | NUMBER	      |
+-------------------------------
+
+Where LAT_N is the northern latitude and LONG_W is the western longitude.
+
+*/
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE NOT RIGHT(CITY, 1) IN ('A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u')
+    AND NOT LEFT(CITY, 1) IN ('A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u');
---
+++ sql/basic-select/4_Select_By_ID.sql
@@ -0,0 +1,21 @@
+/*
+Query all columns for a city in CITY with the ID 1661.
+
+The CITY table is described as follows:
+
+	      CITY
+ ______________ ______________
+| Field	       | Type	      |
+-------------------------------
+| ID	       | NUMBER	      |
+| CITY	       | VARCHAR2(17) |
+| COUNTRYCODE  | VARCHAR2(3)  |
+| DISTRICT     | VARCHAR2(20) |
+| POPULATION   | NUMBER	      |
+-------------------------------
+
+*/
+
+SELECT *
+FROM CITY
+WHERE ID = 1661;
