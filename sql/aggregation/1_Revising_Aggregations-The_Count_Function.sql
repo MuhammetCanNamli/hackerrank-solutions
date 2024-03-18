609d236203f57ced8e5b103c4a6c9f3e913ccc58
Can Namlı <78431281+MuhammetCanNamli@users.noreply.github.com>
2024-03-18 12:00:55 +0300

    Update 1_Revising_Aggregations-The_Count_Function.sql

--- sql/aggregation/1_Revising_Aggregations-The_Count_Function.sql
+++ sql/aggregation/1_Revising_Aggregations-The_Count_Function.sql
@@ -5,13 +5,13 @@ Query a count of the number of cities in CITY having a Population larger than 10
 	      CITY
  ______________ ______________
 | Field	       | Type	      |
--------------------------------
+ -------------- --------------
 | ID	       | NUMBER	      |
 | CITY	       | VARCHAR2(17) |
 | COUNTRYCODE  | VARCHAR2(3)  |
 | DISTRICT     | VARCHAR2(20) |
 | POPULATION   | NUMBER	      |
--------------------------------
+ -------------- --------------
 
 */
 
