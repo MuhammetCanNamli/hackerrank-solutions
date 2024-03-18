20ead2cda6f4d7f515ebcb9ebd0b07801e77f100
Can Namlı <78431281+MuhammetCanNamli@users.noreply.github.com>
2024-03-18 13:27:53 +0300

    Update Working Platforms and Tables

--- sql/basic-select/7_Weather_Observation_Station_1.sql
+++ sql/basic-select/7_Weather_Observation_Station_1.sql
@@ -6,17 +6,19 @@ The STATION table is described as follows:
 	    STATION
  ______________ ______________
 | Field	       | Type	      |
--------------------------------
+ -------------- --------------
 | ID	       | NUMBER	      |
 | CITY	       | VARCHAR2(21) |
 | STATE	       | VARCHAR2(2)  |
 | LAT_N        | NUMBER	      |
 | LONG_W       | NUMBER	      |
--------------------------------
+ -------------- --------------
 
 Where LAT_N is the northern latitude and LONG_W is the western longitude.
 
 */
+
+-- Working Platforms: DB2, MySQL, Oracle, MSSQL
 
 SELECT CITY, STATE
 FROM STATION;
