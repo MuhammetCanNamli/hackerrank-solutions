7895169d1acc12608d4552cad65c5f97b67ec855
Can Namlı <78431281+MuhammetCanNamli@users.noreply.github.com>
2024-03-18 12:02:05 +0300

    Update 4_Average_Population.sql

--- sql/aggregation/4_Average_Population.sql
+++ sql/aggregation/4_Average_Population.sql
@@ -5,13 +5,13 @@ Query the average population for all cities in CITY, rounded down to the nearest
 	      CITY
  ______________ ______________
 | Field	       | Type	      |
--------------------------------
+ -------------- --------------
 | ID	       | NUMBER	      |
 | CITY	       | VARCHAR2(17) |
 | COUNTRYCODE  | VARCHAR2(3)  |
 | DISTRICT     | VARCHAR2(20) |
 | POPULATION   | NUMBER	      |
--------------------------------
+ -------------- --------------
 
 */
 
