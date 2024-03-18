4f3edac113995672fe1959c2813dcb9225c1393e
Can Namlı <78431281+MuhammetCanNamli@users.noreply.github.com>
2024-03-18 13:26:11 +0300

    Update Working Platforms and Tables

--- sql/basic-select/6_Japanese_Cities_Names.sql
+++ sql/basic-select/6_Japanese_Cities_Names.sql
@@ -6,15 +6,17 @@ The CITY table is described as follows:
 	      CITY
  ______________ ______________
 | Field	       | Type	      |
--------------------------------
+ -------------- --------------
 | ID	       | NUMBER	      |
 | CITY	       | VARCHAR2(17) |
 | COUNTRYCODE  | VARCHAR2(3)  |
 | DISTRICT     | VARCHAR2(20) |
 | POPULATION   | NUMBER	      |
--------------------------------
+ -------------- --------------
 
 */
+
+-- Working Platforms: DB2, MySQL, Oracle, MSSQL
 
 SELECT NAME 
 FROM CITY
