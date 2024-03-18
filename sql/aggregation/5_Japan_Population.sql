a1bfa785b795e8789da767c544a69b05ebbe5915
Can Namlı <78431281+MuhammetCanNamli@users.noreply.github.com>
2024-03-18 12:02:29 +0300

    Update 5_Japan_Population.sql

--- sql/aggregation/5_Japan_Population.sql
+++ sql/aggregation/5_Japan_Population.sql
@@ -5,13 +5,13 @@ Query the sum of the populations for all Japanese cities in CITY. The COUNTRYCOD
 	      CITY
  ______________ ______________
 | Field	       | Type	      |
--------------------------------
+ -------------- --------------
 | ID	       | NUMBER	      |
 | CITY	       | VARCHAR2(17) |
 | COUNTRYCODE  | VARCHAR2(3)  |
 | DISTRICT     | VARCHAR2(20) |
 | POPULATION   | NUMBER	      |
--------------------------------
+ -------------- --------------
 
 */
 
