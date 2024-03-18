d86c3cf957d2f86e84bae98d91dc335447536c06
Can Namlı <78431281+MuhammetCanNamli@users.noreply.github.com>
2024-03-18 13:56:56 +0300

    Update Working Platforms and Tables

--- sql/basic-select/19_Employee_Names.sql
+++ sql/basic-select/19_Employee_Names.sql
@@ -6,13 +6,13 @@ Input Format
 The Employee table containing employee data for a company is described as follows:
 
  ______________ ______________
-| Column       | Type	      |
--------------------------------
+| Column       | Type         |
+ -------------- --------------
 | employee_id  | Integer      |
-| name	       | String       |
+| name         | String       |
 | months       | Integer      |
 | salary       | Integer      |
--------------------------------
+ -------------- --------------
 
 where employee_id is an employee's ID number, name is their name, months is the total number of months they've been working for the company, and salary is their monthly salary.
 
@@ -20,19 +20,19 @@ where employee_id is an employee's ID number, name is their name, months is the 
 Sample Input
 
  ______________ ______________ ______________ ______________
-| employee_id  | name	      | months       | salary       |
--------------------------------------------------------------
-| 12228	       | Rose	      | 15           | 1968         |
-| 33645	       | Angela       | 1            | 3443         |
-| 45692	       | Frank        | 17           | 1608         |
+| employee_id  | name         | months       | salary       |
+ -------------- -------------- -------------- --------------
+| 12228        | Rose         | 15           | 1968         |
+| 33645        | Angela       | 1            | 3443         |
+| 45692        | Frank        | 17           | 1608         |
 | 56118        | Patrick      | 7            | 1345         |
-| 59725        | Lisa	      | 11           | 2330         |
-| 74197	       | Kimberly     | 16           | 4372         |
-| 78454	       | Bonnie       | 8            | 1771         |
-| 83565	       | Michael      | 6            | 2017         |
+| 59725        | Lisa         | 11           | 2330         |
+| 74197        | Kimberly     | 16           | 4372         |
+| 78454        | Bonnie       | 8            | 1771         |
+| 83565        | Michael      | 6            | 2017         |
 | 98607        | Todd         | 5            | 3396         |
-| 99989        | Joe	      | 9            | 3573         |
--------------------------------------------------------------
+| 99989        | Joe          | 9            | 3573         |
+ -------------- -------------- -------------- --------------
 
 
 Sample Output
@@ -48,6 +48,8 @@ Patrick
 Rose
 Todd
 */
+
+-- Working Platforms: DB2, MySQL, Oracle, MSSQL
 
 SELECT name 
 FROM Employee
