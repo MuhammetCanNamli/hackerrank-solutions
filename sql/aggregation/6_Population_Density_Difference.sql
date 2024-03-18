0c838e49d5eab488b125a41b28ce0f79f2a425cb
Can Namlı <78431281+MuhammetCanNamli@users.noreply.github.com>
2024-03-18 12:02:55 +0300

    Update 6_Population_Density_Difference.sql

--- sql/aggregation/6_Population_Density_Difference.sql
+++ sql/aggregation/6_Population_Density_Difference.sql
@@ -5,13 +5,13 @@ Query the difference between the maximum and minimum populations in CITY.
 	      CITY
  ______________ ______________
 | Field	       | Type	      |
--------------------------------
+ -------------- --------------
 | ID	       | NUMBER	      |
 | CITY	       | VARCHAR2(17) |
 | COUNTRYCODE  | VARCHAR2(3)  |
 | DISTRICT     | VARCHAR2(20) |
 | POPULATION   | NUMBER	      |
--------------------------------
+ -------------- --------------
 
 */
 
